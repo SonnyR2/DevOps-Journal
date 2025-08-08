9a602ad5cd8f3e84a380f8dd8e394cefe87b8359
SonnyR2 <sonnylrmc@gmail.com>
2025-08-08 18:45:00 -0400

    changed Github Actions for auto deployed eks

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -22,9 +22,9 @@ resource "aws_eks_node_group" "journal-node" {
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids
   scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 2
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 
   instance_types = ["t2.micro"]
