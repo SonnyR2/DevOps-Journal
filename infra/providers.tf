1be86f96dbad38b42c61bb3390f10da8121cfaf1
SonnyR2 <sonnylrmc@gmail.com>
2025-08-10 20:34:21 -0400

    add infra backend

--- infra/providers.tf
+++ infra/providers.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tfstoring123"     
+    key            = "test/terraform.tfstate"  # Path inside the bucket for state file
+    region         = "us-east-1"          
+    use_lockfile   = true
+    encrypt        = true                        
+  }
+}
